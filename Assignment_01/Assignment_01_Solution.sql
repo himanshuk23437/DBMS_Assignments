56038f0612698e9e90157d26d0544367165bc6f8
Himanshu Kumar <himanshukumar23437@gmail.com>
2026-01-29 22:52:43 +0530

    Update Student table structure and data
    Modified the Student table by adding columns City and Semester, renaming Phone to MobileNo, and deleting the Course table. Inserted multiple student records and updated existing entries, including age and department modifications.

--- Assignment_01/Assignment_01_Solution.sql
+++ Assignment_01/Assignment_01_Solution.sql
@@ -1,4 +1,4 @@
--- Creating a table Student
+-- Cteating Required Tables according to Question:-
 CREATE TABLE Student (
   RollNo INT ,
   Name VARCHAR(50),
@@ -6,62 +6,38 @@ CREATE TABLE Student (
   Age INT,
   Phone VARCHAR(15)
 );
-
-
-
--- Creating a table Course
 CREATE TABLE Course (
   CourseID INT ,
   CourseName VARCHAR(50),
   Credits INT
 );
 
-
--- Adding a new column City in table Student
+-- Modifying Table according to Question:-
 ALTER TABLE Student ADD City VARCHAR(20);
-
--- Adding a new column Semester in table Student
 ALTER TABLE Student ADD Semester INT;
+ALTER TABLE Student RENAME COLUMN Phone TO MobileNo; 
 
--- Renaming column Phone to MobileNo in table Student.
-ALTER TABLE Student RENAME COLUMN Phone TO MobileNo;
-
-
--- Deleting (drop) the table Course                                  
+-- Deleting the Course Table
 DROP TABLE Course;
 
-
--- Inserting 5 student records into Student
+-- Inserting data into Table
 INSERT INTO Student VALUES (221, 'Sanskar', 'CSE', 21, '7698521455', 'Muzaffarpur', 5);
 INSERT INTO Student VALUES (598, 'Amarjeet', 'ME', 20, '9112233445', 'Calcutta', 5);
 INSERT INTO Student VALUES (564, 'Himanshu', 'EEE', 20, '6112365478', 'BiharSharif', 6);
 INSERT INTO Student VALUES (258, 'Ashu', 'ME', 22, '8001452369', 'Sitamarhi', 8);
 INSERT INTO Student VALUES (991, 'Anand', 'CSE', 18, '7225236545', 'Vaishali', 5);
 
-
--- Displaying all records from Student.
+-- Displaying Table details according to Question
 SELECT * FROM Student;
-
--- Displaying only RollNo and Name from Student
-SELECT RollNo , Name FROM Student;
-
--- Displaying all students from department "CSE"                                                                                                              
-SELECT * FROM Student WHERE Dept='CSE';
-
--- Displaying all students whose age is greater than 20.                                                                           
+SELECT RollNo , Name FROM Student;                                                                                                         
+SELECT * FROM Student WHERE Dept='CSE';                                                                         
 SELECT * FROM Student WHERE Age > 20 ;
 
--- Updating the department of RollNo = 564 to "ECE".                              
+-- Modifying Table Entries 
 UPDATE Student SET Dept = 'ECE' WHERE RollNo = 564;
-
--- Updating the city of student "Ashu" to "Patna"
 UPDATE student SET city = 'Patna' WHERE name = 'Ashu';
-
--- Increasing age of all students by 1 year
 UPDATE Student SET Age=Age+1;
-
--- Deleting record of student whose RollNo = 991
 DELETE FROM Student WHERE RollNo = 991;
 
--- Deleting all records from Student (table should remain)
+-- Deleting Yable contents
 DELETE FROM Student;
